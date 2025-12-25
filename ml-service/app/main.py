3ef3b17e045bc4cdff579c6693f0b9fd1ccd3b1e
Mohit Choudhari <mohitchoudhari162@gmail.com>
2025-12-25 17:50:22 +0530

    Tiling of the image

--- ml-service/app/main.py
+++ ml-service/app/main.py
@@ -3,6 +3,7 @@ import os
 import uuid
 
 from utils.image_utils import read_image_info
+from utils.tiler import tile_image
 
 app = FastAPI(title= "Autoslide ml service")
 
@@ -32,4 +33,30 @@ async def upload_image(file: UploadFile = File(...)):
         "message": "Image uploaded successfully",
         "filename": filename, 
         "image_info": image_info
+    }
+
+@app.post("/tile-image/{filename}")
+def tile_uploaded_image(filename: str):
+    image_path = os.path.join(UPLOAD_DIR, filename)
+
+    if not os.path.exists(image_path):
+        return {"error": "File not found"}
+    
+    tiles_dir = os.path.join("uploads", "tiles", filename.split(".")[0] )
+
+    result = tile_image(
+        image_path = image_path,
+        output_dir = tiles_dir,
+        tile_size = 256,
+        overlap = 0
+    )
+
+    return {
+        "message": "Image tiled successfully",
+        "tiling_info": {
+            "image_width": result["image_width"],
+            "image_height": result["image_height"],
+            "tile_size": result["tile_size"],
+            "num_tiles": result["num_tiles"]
+        }
     }
---
+++ ml-service/utils/tiler.py
@@ -0,0 +1,52 @@
+import os
+from PIL import Image
+
+def tile_image(
+    image_path: str, 
+    output_dir: str,
+    tile_size: int = 256,
+    overlap: int = 0
+):
+    """
+    Split an image into tiles and save them.
+
+    Returns metadata about tiling.
+    """
+    os.makedirs(output_dir, exist_ok=True)
+
+    img = Image.open(image_path)
+    width, height = img.size
+
+    tiles = []
+    tile_id = 0
+
+    step = tile_size - overlap
+
+    for y in range(0, height - tile_size + 1, step):
+        for x in range(0, width - tile_size +1, step):
+            tile = img.crop((x, y, x+tile_size, y+tile_size))
+
+            tile_filename = f"tile_{tile_id}.png"
+            tile_path = os.path.join(output_dir, tile_filename)
+            tile.save(tile_path)
+
+            tiles.append({
+                "tile_id": tile_id,
+                "x": x,
+                "y": y,
+                "file_path": tile_path
+            })
+
+            tile_id += 1
+
+        img.close()
+
+        return {
+            "image_width": width,
+            "image_height": height,
+            "tile_size": tile_size,
+            "overlap": overlap,
+            "num_tiles": len(tiles),
+            "tiles": tiles
+        }
+ 
