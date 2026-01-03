dc0f741689bf12c758cbdf92fbfa83c4fcbc5c31
Mohit Choudhari <mohitchoudhari162@gmail.com>
2026-01-03 22:32:52 +0530

    Middleware added to the route

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { register, login} = require("../controllers/auth.controller");
+const authMiddleware = require("../middleware/auth.middleware");
 
 router.post("/register", register);
-router.post("/login", login);
+router.post("/login", authMiddleware, login);
 
 module.exports = router;
